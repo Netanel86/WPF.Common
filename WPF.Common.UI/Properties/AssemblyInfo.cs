aca7d6a6c1e9773c9f2796bfbc3cd376e041a85d
Iting Netanel <iting86@gmail.com>
2017-01-18 00:06:46 +0200

    Upgraded to Visual Studio 2015

--- WPF.Common.UI/Properties/AssemblyInfo.cs
+++ WPF.Common.UI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WPF.Common.UI")]
-[assembly: AssemblyCopyright("Netanel Iting ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38c8cbbc-e96b-4d53-abf6-da66fa0ca9a1")]
+[assembly: Guid("d5a86301-1950-4a3d-a3ee-9d3f40826746")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
